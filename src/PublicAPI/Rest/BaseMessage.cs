b64c77c52c58a4a52f4a3191dca1812d5f3d8514
Renato Nagy <renato_nagy@epam.com>
2023-03-27 13:37:43 +0200

    Added doc.

--- src/PublicAPI/Rest/BaseMessage.cs
+++ src/PublicAPI/Rest/BaseMessage.cs
@@ -5,5 +5,8 @@
 /// </summary>
 public abstract class BaseMessage
 {
+    /// <summary>
+    /// Gets or sets the correlation id of the message. Used for tracing.
+    /// </summary>
     public Guid CorrelationId { get; set; }
 }
--- src/PublicAPI/Rest/Response/ErrorDetails.cs
+++ src/PublicAPI/Rest/Response/ErrorDetails.cs
@@ -1,11 +1,22 @@
 ﻿namespace PublicAPI.Rest.Response;
 
 using System.Text.Json;
+using PublicAPI.Middleware;
 
+/// <summary>
+/// Message if an error is being thrown in the application.
+/// Used by <see cref="ExceptionMiddleware"/>.
+/// </summary>
 public class ErrorDetails
 {
+    /// <summary>
+    /// Gets or sets the status code for the error.
+    /// </summary>
     public int StatusCode { get; set; }
 
+    /// <summary>
+    /// Gets or sets the text message of the error.
+    /// </summary>
     public string? Message { get; set; }
 
     /// <inheritdoc/>
